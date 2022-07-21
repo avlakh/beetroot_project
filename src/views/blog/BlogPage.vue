92afb4f5641dc3d4ec4a32fc27eff7de2018778a
Artem Vlakh <vlakhartem@gmail.com>
2022-07-21 14:42:29 +0200

    blog view/page responsive improved

--- src/views/blog/BlogPage.vue
+++ src/views/blog/BlogPage.vue
@@ -1,17 +1,17 @@
 <template>
-    <section class="blog_home container">
-            <div class="blog_home_main_text">
-                <h2>{{item.title}}</h2>
-                <p><span class="blog_home_author_name">{{item.author}}</span><span><time>{{item.publishedAt}}</time></span></p>
-            </div>
-            <div class="blog_home_img">
-                <img :src="item.urlToImage" :alt="item.title">
-            </div>
-        </section>
-        <section class="blog_text text_container">
-            <p>{{item.content}}</p>
-            <router-link to="/blog" class="round_btn">Back to news</router-link>
-        </section>
+	<section class="blog_home container">
+			<div class="blog_home_main_text">
+				<h2>{{item.title}}</h2>
+				<p><span class="blog_home_author_name">{{item.author}}</span><span><time>{{item.publishedAt}}</time></span></p>
+			</div>
+			<div class="blog_home_img">
+				<img :src="item.urlToImage" :alt="item.title">
+			</div>
+		</section>
+		<section class="blog_text text_container">
+			<p>{{item.content}}</p>
+			<router-link to="/blog" class="round_btn">Back to news</router-link>
+		</section>
 </template>
 
 <script>
@@ -19,21 +19,21 @@
 import axios from 'axios';
 
 export default {
-    data(){
-        return {
-            item: {}
-        }
-    }, created(){
-        let url = `https://newsapi.org/v2/everything?q=${this.$route.params.title}&searchIn=title&apiKey=8de73a80555649889b993ecdb6eb1844`;
-        axios 
-            .get(url)
-            .then(resp=>{
-                this.item = resp.data.articles[0]
-            })
-    }
+	data(){
+		return {
+			item: {}
+		}
+	}, created(){
+		let url = `https://newsapi.org/v2/everything?q=${this.$route.params.title}&searchIn=title&apiKey=8de73a80555649889b993ecdb6eb1844`;
+		axios 
+			.get(url)
+			.then(resp=>{
+				this.item = resp.data.articles[0]
+			})
+	}
 }
 </script>
 
 <style lang="scss" scoped>
-    @import '@/assets/css/pages/blog_page.scss'
+	@import '@/assets/css/pages/blog_page.scss'
 </style>
--- src/views/blog/BlogView.vue
+++ src/views/blog/BlogView.vue
@@ -1,42 +1,46 @@
 <template>
-    <section class="blog_home container">
-        <template v-for="(item, index) in list" :key="index">
-            <template v-if="index === 0">
-                <div class="blog_home_main_text">
-                    <h2>{{item.title}}</h2>
-                    <p><span class="blog_home_author_name">{{item.author}}</span><span>{{item.publishedAt}}</span></p>
-                </div>
-                <div class="blog_home_img">
-                    <img :src="item.urlToImage" :alt="item.title">
-                </div>
-                <div class="blog_home_descr">
-                    <p v-html="item.description"/>
-                    <router-link :to="{name: 'blog-page', params:{title: item.title}}">Read More</router-link>
-                </div>
-            </template>
-        </template>
-    </section>
-    <section class="our_blog">
-        <div class="container">
-            <h2>Our Blog</h2>
-            <div class="blog_card_wrap">
-                <template v-for="(item, index) in list" :key="index">
-                    <div class="blog_card" v-if="index > 0">
-                        <router-link :to="{name: 'blog-page', params:{title: item.title}}">
-                        <div class="blog_card_img">
-                            <img :src="item.urlToImage" :alt="item.title">
-                        </div>
-                        <h6>{{item.publishedAt}}</h6>
-                        <h5>{{item.title}}</h5>
-                        <p v-html="item.description"/>
-                        <router-link :to="{name: 'blog-page', params:{title: item.title}}" class="arrow_btn">Read More</router-link>
-                        </router-link>
-                    </div>
-                </template>
-            </div>
-        </div>
-        <blog-pagination @goToPage="goToPage" :total="total" :page="page"/>
-    </section>
+		<section class="blog_home container">
+			<template v-for="(item, index) in list" :key="index">
+				<template v-if="index === 0">
+					<div class="blog_home_main_text">
+						<router-link :to="{name: 'blog-page', params:{title: item.title}}">
+							<h2>{{item.title}}</h2>
+							<p><span class="blog_home_author_name">{{item.author}}</span><span>{{item.publishedAt}}</span></p>
+						</router-link>
+					</div>
+					<div class="blog_home_img">
+						<router-link :to="{name: 'blog-page', params:{title: item.title}}">
+							<img :src="item.urlToImage" :alt="item.title">
+						</router-link>
+					</div>
+					<div class="blog_home_descr">
+						<p v-html="item.description"></p>
+						<router-link :to="{name: 'blog-page', params:{title: item.title}}">Read More</router-link>
+					</div>
+				</template>
+			</template>
+		</section>
+		<section class="our_blog">
+			<div class="container">
+				<h2>Our Blog</h2>
+				<div class="blog_card_wrap">
+					<template v-for="(item, index) in list" :key="index">
+						<div class="blog_card" v-if="index > 0">
+							<router-link :to="{name: 'blog-page', params:{title: item.title}}">
+								<div class="blog_card_img">
+									<img :src="item.urlToImage" :alt="item.title">
+								</div>
+								<h6>{{item.publishedAt}}</h6>
+								<h5>{{item.title}}</h5>
+								<p v-html="item.description"></p>
+							</router-link>
+							<router-link :to="{name: 'blog-page', params:{title: item.title}}" class="arrow_btn">Read More</router-link>
+						</div>
+					</template>
+				</div>
+			</div>
+			<blog-pagination @goToPage="goToPage" :total="total" :page="page"/>
+		</section>
 </template>
 
 <script>
@@ -45,53 +49,52 @@ import axios from 'axios'
 import BlogPagination from '@/components/blog/BlogPagination.vue';
 
 export default {
-    name: 'BlogView',
-    components: {
-        BlogPagination
-    },
-    data () {
-        return {
-            list: [],
-            page: 1,
-            total: 0
-        }
-    },
-    computed: {
-        dateFormatted(){
-            const d = new Date();
-            let month = d.getMonth()+1;
-            let day = d.getDate();
-            return d.getFullYear()+'-'+(month < 10 ? '0'+month : month)+'-'+(day < 10 ? '0'+day : day)
-        }
-    },
-    created () {
-        this.fetchData()
-    },
-    methods: {
-        fetchData(){
-            let url = 'https://newsapi.org/v2/everything?q=design&apiKey=8de73a80555649889b993ecdb6eb1844';
-            url += '&from='+this.dateFormatted
-            url += '&to='+this.dateFormatted
-            url += '&language=en'
-            url += '&pageSize=19'
-            url += '&page='+this.page  
-            axios
-                .get(url)
-                .then(resp=>{
-                    this.list = resp.data.articles;
-                    // this.total = Math.ceil(resp.data.totalResults / 18)
-                    this.total = 4;
-                })
-        },
-        goToPage(new_page) {
-            this.page = new_page;
-            this.fetchData();
-        }
-        
-    }
+	name: 'BlogView',
+	components: {
+		BlogPagination
+	},
+	data () {
+		return {
+			list: [],
+			page: 1,
+			total: 0
+		}
+	},
+	computed: {
+		dateFormatted(){
+			const d = new Date();
+			let month = d.getMonth()+1;
+			let day = d.getDate();
+			return d.getFullYear()+'-'+(month < 10 ? '0'+month : month)+'-'+(day < 10 ? '0'+day : day)
+		}
+	},
+	created () {
+		this.fetchData()
+	},
+	methods: {
+		fetchData(){
+			let url = 'https://newsapi.org/v2/everything?q=design&apiKey=8de73a80555649889b993ecdb6eb1844';
+			url += '&from='+this.dateFormatted
+			url += '&to='+this.dateFormatted
+			url += '&language=en'
+			url += '&pageSize=19'
+			url += '&page='+this.page  
+			axios
+				.get(url)
+				.then(resp=>{
+					this.list = resp.data.articles;
+					// this.total = Math.ceil(resp.data.totalResults / 18)
+					this.total = 4;
+				})
+		},
+		goToPage(new_page) {
+			this.page = new_page;
+			this.fetchData();
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
-    @import '@/assets/css/pages/blogs.scss'
+	@import '@/assets/css/pages/blogs.scss';
 </style>
